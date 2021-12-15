2ca774836a697bc546f8adb89f3f5677da9571e9
jesusjuvinao <jesus.juvinao@smartaccountingonline.com>
2021-12-15 16:57:56 -0500

    push code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { GlobalStyle } from '../public/styles/GlobalStyle'
+import '../public/styles/App.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <GlobalStyle />
+     <Component {...pageProps} />
+    </>
+     )
 }
 
 export default MyApp
