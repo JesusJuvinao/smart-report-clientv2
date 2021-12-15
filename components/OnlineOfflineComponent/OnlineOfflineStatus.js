0fb17c5ec4e183c6b8ab29fe74c32b50293bd14d
jesusjuvinao <jesus.juvinao@smartaccountingonline.com>
2021-12-15 18:58:08 -0500

    push compoents

--- components/OnlineOfflineComponent/OnlineOfflineStatus.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Offline, Online } from 'react-detect-offline'
-
-export const UserOnlineStatusComponent = () => (
-    <div>
-        <Online>Only shown when youre online</Online>
-        <Offline>Only shown offline (surprise!)</Offline>
-    </div>
-)
