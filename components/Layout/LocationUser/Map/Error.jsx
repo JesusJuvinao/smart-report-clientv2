82d334dcfdb08227d1fb5d16c7981cd74c8a7bdb
jesusjuvinao <jesus.juvinao@smartaccountingonline.com>
2021-12-15 17:57:10 -0500

    asdasd

--- components/Layout/LocationUser/Map/Error.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Error = () => {
-  return (
-        <div>
-            Ha ocurrido un error al mostrar el mapa
-        </div>
-  )
-}
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,7 @@
-import { GlobalStyle } from '../public/styles/GlobalStyle'
-import '../public/styles/App.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return(
-    <>
-    <GlobalStyle />
-     <Component {...pageProps} />
-    </>
-     )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
