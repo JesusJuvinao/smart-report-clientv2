25dedca63cf65f7b3119251e0650a91dbb9c024d
jesusjuvinao <jesus.juvinao@smartaccountingonline.com>
2021-12-15 17:39:20 -0500

    ukjsbdkfs

--- container/Home/index.jsx
+++ container/Home/index.jsx
@@ -18,7 +18,9 @@ export const IndexC = () => {
       <PQR />
       <Item />
       <OnBanner height='18.25rem' title='See what QuickBooks can do for your business.' color={BGColor} bgColor={SECColor} width='220px' size='2.125rem' Button TextBtn='Buy now' />
-      </Container>
+
+    </Container>
+
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-import { Container } from '../container/Home/styled'
+import Head from 'next/head'
+import Image from 'next/image'
+import { IndexC } from '../container/Home/'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    return <Container>Hola</Container>
+  return (
+    <IndexC />
+  )
 }
