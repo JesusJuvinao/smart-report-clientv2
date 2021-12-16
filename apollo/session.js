788ed158e334ed85c8a90239e46f727afbd22f6a
jesusjuvinao <jesus.juvinao@smartaccountingonline.com>
2021-12-16 09:06:39 -0500

    push cpde

--- apollo/session.js
+++ apollo/session.js
@@ -1,15 +1,13 @@
 import { withIronSession } from 'next-iron-session'
 
 export default function withSession (handler) {
-    const ttl = new Date(Date.now() + 2647483647)
     return withIronSession(handler, {
         password: process.env.SESSION_KEY,
         // Session Name
         cookieName: process.env.SESSION_NAME,
         cookieOptions: {
-            path: "/",
-            secure: process.env.NODE_ENV === "production",
-            maxAge: ttl
+            secure: process.env.NODE_ENV === 'production' ? true : false
         }
     })
+
 }
