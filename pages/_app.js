7948f4cf59d5ab8d579b9194b90a5efae2ba0eb9
jesusjuvinao <jesus.juvinao@smartaccountingonline.com>
2021-12-15 19:07:07 -0500

    push context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import React from 'react'
+import '../public/styles/App.css'
+import PropTypes from 'prop-types'
+import { Maintenance } from '../components/Maintenance'
+import AuthProvider from '../context'
+import { GlobalStyle } from '../public/styles/GlobalStyle'
+const production = true
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <AuthProvider>
+    <GlobalStyle />
+    {production ? <Component {...pageProps} /> : <Maintenance />}
+    </AuthProvider>
+     )
 }
 // vamos 
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styled from 'styled-components'
-import styles from '../styles/Home.module.css'
 import { IndexC } from '../container/Home';
 
 export default function Home() {
