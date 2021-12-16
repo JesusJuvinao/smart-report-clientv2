39d18579c1964203d1caf4789e78d2ac38537ff6
jesusjuvinao <jesus.juvinao@smartaccountingonline.com>
2021-12-16 09:12:42 -0500

    config cookie

--- apollo/session.js
+++ apollo/session.js
@@ -1,13 +1,15 @@
+// this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
 import { withIronSession } from 'next-iron-session'
 
-export default function withSession (handler) {
-    return withIronSession(handler, {
-        password: process.env.SESSION_KEY,
-        // Session Name
-        cookieName: process.env.SESSION_NAME,
-        cookieOptions: {
-            secure: process.env.NODE_ENV === 'production' ? true : false
-        }
-    })
-
+export default function withSession(handler) {
+  return withIronSession(handler, {
+    password: process.env.SESSION_KEY,
+    // Session Name
+    cookieName: process.env.SESSION_NAME,
+    cookieOptions: {
+      // the next line allows to use the session in non-https environments like
+      // Next.js dev mode (http://localhost:3000)
+      secure: process.env.NODE_ENV === 'production' ? true : false,
+    },
+  })
 }
