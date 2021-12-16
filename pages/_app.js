10eda00414413029a51864f89594ab8a06a19e6e
jesusjuvinao <jesus.juvinao@smartaccountingonline.com>
2021-12-15 20:45:13 -0500

    push config apollo

--- pages/_app.js
+++ pages/_app.js
@@ -5,24 +5,18 @@ import { Maintenance } from '../components/Maintenance'
 import Context from '../context'
 import { GlobalStyle } from '../public/styles/GlobalStyle'
 import {
-  createHttpLink,
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache
+  ApolloProvider
 } from '@apollo/client'
+import { useApollo } from '../apollo/apolloClient'
 const production = true
 
 function MyApp({ Component, pageProps }) {
-  const client = new ApolloClient({
-      cache: new InMemoryCache(),
-      link: createHttpLink({ uri: '/api/graphql' })
-  })
-
+  const apolloClient = useApollo(pageProps)
   return(
     <Context>
-       <ApolloProvider client={client}>
+       <ApolloProvider client={apolloClient}>
     <GlobalStyle />
-    {production ? <Component {...pageProps} /> : <Maintenance />}
+    {<Component {...pageProps} /> }
        </ApolloProvider>
     </Context>
      )
