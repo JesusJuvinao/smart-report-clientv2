6dd7a20709fc3e3ec390141bbae396f9e0725165
jesusjuvinao <jesus.juvinao@smartaccountingonline.com>
2021-12-15 19:26:47 -0500

    build conection

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-import styled from 'styled-components'
+import { gql, useQuery } from '@apollo/client';
 import { IndexC } from '../container/Home';
-
 export default function Home() {
-  return (<IndexC />)
+  const GET_BEASTS = gql`
+  query sayHello {
+    sayHello
+}`;
+  const { loading, error, data } = useQuery(GET_BEASTS);
+  console.log(loading, error, data)
+  if (loading) return <div>LOADING</div>
+  return (
+     <IndexC />
+     )
 }
-
-const Container = styled.div`
-  background: red;
-  padding: 30px;
-`
