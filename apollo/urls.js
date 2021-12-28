747e92563014677983f054417a1b39d6bae1a576
Jesus juvinao <juvinaojesusd@gmail.com>
2021-12-27 19:01:11 -0500

    cambiando a la rama developemnt

--- apollo/urls.js
+++ apollo/urls.js
@@ -1,2 +1,2 @@
-export const URL_BASE = 'http://localhost:3000/api/'
+export const URL_BASE = 'http://http:/143.198.148.145/api/'
 // export const URL_BASE = ' https://9331-181-206-125-176.ngrok.io/api/'
