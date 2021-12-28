01fa3b2c3b0db04251d83ee59e67a180256ef070
Jesus juvinao <juvinaojesusd@gmail.com>
2021-12-27 19:21:18 -0500

    cambiando a la rama developemnt

--- apollo/urls.js
+++ apollo/urls.js
@@ -1,2 +1,2 @@
-export const URL_BASE = 'http://http:/143.198.148.145/api/'
+export const URL_BASE = 'http://smartreportz.com/api/'
 // export const URL_BASE = ' https://9331-181-206-125-176.ngrok.io/api/'
